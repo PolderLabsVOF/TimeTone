9dd64f3a2ddcf2a8c3c5135c1452e297ab49a401
Berk de Rooij <berk@polderlabs.io>
2026-09-14 16:50:02 +0200

    chore(release): bump version to 0.3.4
    Promote 0.3.4-rc.1 to stable. Update VERSION, web/package.json,
    firmware/main/timekeep.h, and CHANGELOG.md from 0.3.4-rc.1 to 0.3.4.

--- firmware/main/timekeep.h
+++ firmware/main/timekeep.h
@@ -6,7 +6,7 @@
 #define TK_MAX_EMPLOYEES 32
 #define TK_MAX_EVENTS 48
 #define TK_MAX_CODE_REQUESTS 12
-#define TK_FIRMWARE_VERSION "0.3.4-rc.1"
+#define TK_FIRMWARE_VERSION "0.3.4"
 
 typedef struct {
     char ssid[33];
